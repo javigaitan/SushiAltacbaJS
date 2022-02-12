48a4c1ad39d291290d383efbc0d6163103b1a328
Gaitan Javier Emanuel <95413204+javigaitan@users.noreply.github.com>
2022-02-12 19:50:01 -0300

    correcion de lsita

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,33 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+(function ($) {
+    // Instantiate MixItUp:
+    $('#Container').mixItUp();
+
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".sidenav a").on('click', function(event) {
+        var hash = this.hash;
+        if( hash ) {
+          event.preventDefault();
+          $('html, body').animate({
+              scrollTop: $(hash).offset().top
+          }, 900, function(){
+              window.location.hash = hash;
+          });
+        }
+
+    });
+    
+})(jQuery);
+
+
+
 //Evento sobre reserva
 
 
@@ -11,7 +41,7 @@
 }())
 
 
-//Constructor de li
+
 
 const input = document.querySelector("input");
 const addBtn = document.querySelector(".btn-add");
